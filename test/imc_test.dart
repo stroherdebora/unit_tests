93b0f2054adc96cbf14a422f3530f8fb6fc75309
Débora Ströher <stroherdebora.com>
2024-02-20 21:03:08 -0300

    life cycle

--- test/imc_test.dart
+++ test/imc_test.dart
@@ -3,6 +3,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_tests/imc.dart';
 
 void main() {
+// Você pode usar as funções setUp() e tearDown() para compartilhar
+// código entre testes. O retorno de chamada setUp() será executado antes de cada
+// teste em um grupo ou conjunto de testes, e tearDown() será executado depois.
+// tearDown() será executado mesmo se um teste falhar, para garantir que ele tenha a
+// chance de se limpar.
+  setUp(() => null);
+  tearDown(() => null);
+  setUpAll(() => null);
+  tearDownAll(() => null);
+
   test('Deve efetuar o cálculo do IMC', () {
     final result = calcIMC(1.78, 64);
     expect(result, equals(20.199469763918696));
