690be0e1fd49d4348385fe5eb986669d94e504a2
deborastroher <debora.stroher@ucsbr.com>
2024-02-21 17:39:48 -0300

    other matchers

--- test/imc_test.dart
+++ test/imc_test.dart
@@ -3,11 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_tests/imc.dart';
 
 void main() {
-// Você pode usar as funções setUp() e tearDown() para compartilhar
-// código entre testes. O retorno de chamada setUp() será executado antes de cada
-// teste em um grupo ou conjunto de testes, e tearDown() será executado depois.
-// tearDown() será executado mesmo se um teste falhar, para garantir que ele tenha a
-// chance de se limpar.
+  // Você pode usar as funções setUp() e tearDown() para compartilhar
+  // código entre testes. O retorno de chamada setUp() será executado antes de cada
+  // teste em um grupo ou conjunto de testes, e tearDown() será executado depois.
+  // tearDown() será executado mesmo se um teste falhar, para garantir que ele tenha a
+  // chance de se limpar.
   setUp(() => null);
   tearDown(() => null);
   setUpAll(() => null);
@@ -23,10 +23,15 @@ void main() {
 
     // Assert
     expect(result, equals(20.199469763918696));
+    // Other matchers
+    expect(result, isA<double>());
+    expect(result, greaterThan(20));
+    expect(result, isPositive);
+    expect(result.toString(), matches(RegExp(r'\d')));
   });
 
-// Tests can be grouped together using the group() function.
-// Each group's description is added to the beginning of its test's descriptions.
+  // Tests can be grouped together using the group() function.
+  // Each group's description is added to the beginning of its test's descriptions.
   group('Exceções de parãmetros: ', () {
     test('Deve lançar uma exceção se altura for < 1', () {
       expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
